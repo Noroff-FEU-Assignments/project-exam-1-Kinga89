37d9033605b649c5beb6d600584dcc275783b7d2
Kinga89 <kinga-kot@hotmail.com>
2022-09-17 17:18:49 +0200

    blog specific page

--- js/post.js
+++ js/post.js
@@ -20,21 +20,49 @@ async function fetchPostDetails() {
     const result = await details.json();
     console.log(result);
     document.title = `${result.title.rendered}`;
+
+    const parser = new DOMParser();
+    const content1 = result;
+    const parsedDocument = parser.parseFromString(
+      content1.content.rendered,
+      "text/html"
+    );
+    const img = parsedDocument.querySelector("img");
+    console.log(img.attributes.src.nodeValue);
+
+    const parserTxt = new DOMParser();
+    const content2 = result;
+    const parsedDocumentTxt = parserTxt.parseFromString(
+      content2.content.rendered,
+      "text/html"
+    );
+    const txt = parsedDocumentTxt.querySelectorAll("p");
+ 
       
-    postContainer.innerHTML = `
+        let newTxt = Array.from(txt)
+      console.log(newTxt);
+      
+      for (let i = 0; i < newTxt.length; i++){
+          console.log(newTxt[i].innerHTML);
+
+          
+          postContainer.innerHTML = `
         <h1>${result.title.rendered}</h1>
         <p>${result.date}</p>
         <p>Written by ${result.date}</p>
         <div class="post-image">
-        <img src="${result}" alt="">
+        <img src="${img.attributes.src.nodeValue}" alt="">
         </div>
         <div class="post-text">
-        <p>${result.content.rendered}</p>
+        <p>${newTxt[0].innerHTML}</p>
         </div>
-        <div class="comment-section"><p>${result._links.replies[0].href}</p></div>
         `;
-
- 
+          
+          const textPost = document.querySelectorAll(".post-text");
+          textPost.innerHTML = newTxt[i].innerText;
+        console.log (textPost)
+      }
+      
   } catch (error) {
     postContainer.innerHTML = `<div id="error_msg">An error occured when calling the API </div>`;
     console.log(error);
