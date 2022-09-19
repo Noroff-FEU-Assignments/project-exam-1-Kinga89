0ec0336d14007cb9ed4859dfa1c075018ab49308
Kinga89 <kinga-kot@hotmail.com>
2022-09-19 21:21:09 +0200

    resized about page

--- js/about.js
+++ js/about.js
@@ -2,4 +2,13 @@ import { goBack } from "./utils.js";
 const previousPage = document.querySelector(".fa-arrow-left");
 previousPage.addEventListener("click", goBack);
 
-document.title = "About | Urban Planning Blog"
+document.title = "About | Urban Planning Blog";
+
+const hamburgerOpen = document.querySelector(".fa-bars");
+const hamburgerClose = document.querySelector(".fa-times");
+const navMobile = document.querySelector("nav ul");
+
+import { hamburgerMenuOpen,hamburgerMenuClose } from "./utils.js";
+
+hamburgerOpen.addEventListener("click", hamburgerMenuOpen);
+hamburgerClose.addEventListener("click", hamburgerMenuClose);
